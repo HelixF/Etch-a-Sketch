73d89ff07ba42a9a834a1591f77a777560aa87f0
Florian Soller <flsoller@gmail.com>
2020-04-16 23:50:05 +0200

    Finished logic for grid generation. Layout + Styling fixed.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Select container to append childen later (div's).
+// Select container to append gridCell(s) later (as children).
 const container = document.getElementById('container');
+const gridHeight = 600;
+const gridWidth = 600;
+const defaultGridSize = 32**2;
 
 // TODO: Define grid size based on user input.
-let gridSize = 16**2;
+let gridSize = 32**2;
 
 // Generate div elements based on input and append to container.
 for (i = 0 ; i < gridSize ; i++) {
@@ -16,17 +19,21 @@ for (i = 0 ; i < gridSize ; i++) {
     
 }
 
+//Dynamic grid styling based on gridSize.
+container.style.gridTemplateColumns = 
+'repeat('+ Math.sqrt(gridSize) + ',' + gridWidth / Math.sqrt(gridSize) + 'px)';
+
+container.style.gridTemplateRows = 
+'repeat(' + Math.sqrt(gridSize) +',' + gridHeight / Math.sqrt(gridSize) + 'px)';
+
 
 // Implements the color change when mouse is over target.
 function colorChange (e) {
-    let cell = document.querySelector('.gridCell');
-    event.target.style.backgroundColor = '#f4f4f4';
-    
-    
+    event.target.style.backgroundColor = '#99CED3';   
 }
 
 //Logging
-console.log(document.body);
-console.log(inputNumber);
+//console.log(document.body);
+
 
 
