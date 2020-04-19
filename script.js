c9f41d0d01a1d8f79a8efe7f54d42592f56c8689
Florian Soller <flsoller@gmail.com>
2020-04-19 17:19:51 +0200

    Implement function for "New Grid" button.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Select container to append gridCell(s) later (as children).
 const container = document.getElementById('container');
+
+// Default grid size in px.
 const gridHeight = 600;
 const gridWidth = 600;
-const defaultGridSize = 32**2;
 
-// TODO: Define grid size based on user input.
+// Prompts user to enter a grid size. Deletes old grid.
+let clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', resetGrid);
+
 let gridSize = 32**2;
+
+
 
 // Generate div elements based on input and append to container.
 for (i = 0 ; i < gridSize ; i++) {
@@ -19,6 +25,7 @@ for (i = 0 ; i < gridSize ; i++) {
     
 }
 
+
 //Dynamic grid styling based on gridSize.
 container.style.gridTemplateColumns = 
 'repeat('+ Math.sqrt(gridSize) + ',' + gridWidth / Math.sqrt(gridSize) + 'px)';
@@ -27,13 +34,42 @@ container.style.gridTemplateRows =
 'repeat(' + Math.sqrt(gridSize) +',' + gridHeight / Math.sqrt(gridSize) + 'px)';
 
 
+
 // Implements the color change when mouse is over target.
 function colorChange (e) {
     event.target.style.backgroundColor = '#99CED3';   
 }
 
-//Logging
-//console.log(document.body);
+// Draws new grid based on user input
+function resetGrid (e) {
 
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
 
+    let newGridSize = prompt('How many squares per side do you want? Enter a number (Maximum is 100).') ** 2;
+
+    if ( newGridSize <= 100 ** 2) {
+
+        for (i = 0 ; i < newGridSize ; i++) {
+        let gridCell = document.createElement('div');
+        gridCell.classList = 'gridCell';
+
+        container.appendChild(gridCell);
+
+        // Add Event Listener to trigger color change.
+        gridCell.addEventListener('mouseover', colorChange);
+
+        container.style.gridTemplateColumns = 
+        'repeat('+ Math.sqrt(newGridSize) + ',' + gridWidth / Math.sqrt(newGridSize) + 'px)';
+        
+        container.style.gridTemplateRows = 
+        'repeat(' + Math.sqrt(newGridSize) +',' + gridHeight / Math.sqrt(newGridSize) + 'px)';
+        }
+    }
+    else {
+        alert('This number is too large. Try something less than 100 squares per side.')
+    }
+
+}
 
